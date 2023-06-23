9d901f51edfca97698f99b8e69ae0f8ddbbc0bdc
Jean Carlos Michetti <scan.michetti@voeazul.com.br>
2023-06-23 09:46:17 -0300

    Primeiro Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Papa from 'papaparse';
+import './App.css'; // Importe o arquivo CSS com o estilo criado
 
 function App() {
+  const [csvData, setCsvData] = useState([]);
+  const [totalCol8, setTotalCol8] = useState(0);
+  const [totalCol9, setTotalCol9] = useState(0);
+  const handleFileUpload = (event) => {
+    const file = event.target.files[0];
+    Papa.parse(file, {
+      complete: (results) => {
+        const data = results.data;
+        setCsvData(data);
+        calculateTotal(data);
+        calculateTotalEstimate(data);
+      },
+    });
+  };
+
+  const calculateTotal = (data) => {
+    let total = 0;
+    for (let i = 1; i < data.length; i++) {
+      const value = parseFloat(data[i][7]);
+      if (!isNaN(value)) {
+        total += value;
+      }
+    }
+    setTotalCol8(total);
+  };
+
+  const calculateTotalEstimate = (data) => {
+    let total = 0;
+    for (let i = 1; i < data.length; i++) {
+      const value = parseFloat(data[i][8]);
+      if (!isNaN(value)) {
+        total += value;
+      }
+    }
+    setTotalCol9(total);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Upload de Arquivo CSV</h1>
+      <input type="file" accept=".csv" onChange={handleFileUpload} />
+      <div className="table-container">
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {csvData.map((row, index) => (
+              <tr key={index}>
+                {row.map((cell, cellIndex) => (
+                  <td key={cellIndex}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <p>Total Concluido: {totalCol8}</p>
+      <p>Total Estimado: {totalCol9}</p>
     </div>
   );
 }
